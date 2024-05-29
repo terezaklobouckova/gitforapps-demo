4a0322c5050b190693bda14cdade99a9dd4ef654
terezaklobouckova <58560848+terezaklobouckova@users.noreply.github.com>
2024-05-29 09:59:02 +0200

    Delete src directory

--- src/functions/parse-timestamps/parse-timestamps.code.js
+++
@@ -1,9 +0,0 @@
-function parseTimestamps(itemObj) {
-
-    itemObj.created = itemObj.created ? iml.parseDate(Number(itemObj.created ), 'x') : null;
-    itemObj.lastModified = itemObj.lastModified ? iml.parseDate(Number(itemObj.lastModified ), 'x') : null;
-    itemObj.lastExecutedDate = itemObj.lastExecutedDate ? iml.parseDate(Number(itemObj.lastExecutedDate ), 'x') : null;
-    itemObj.lastUpdatedDate = itemObj.lastUpdatedDate ? iml.parseDate(Number(itemObj.lastUpdatedDate ), 'x') : null;
-    itemObj.createdDate = itemObj.createdDate ? iml.parseDate(Number(itemObj.createdDate ), 'x') : null;
-    return itemObj;
-}
